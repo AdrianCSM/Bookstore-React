db506bc922509ca66959c264527276a4f0cc3826
Cosmin Adrian <cosminadrian218@gmail.com>
2022-05-07 17:02:44 +0300

    Make add and remove functionality

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../redux/books/books';
+
+  const {
+    id,
+    title,
+    author,
+    category,
+  } = props;
+  const dispatch = useDispatch();
+  const remove = () => dispatch(removeBook(id));
 
 function BookItem() {
   return (
     <div className="BookContainer">
       <div className="TitleContainer">
-        <p className="Genre">Category</p>
-        <p className="BookTitle">Title</p>
-        <p className="BookAuthor">Author</p>
-        <button type="button" className="Remove">Remove</button>
+        <p className="Genre" value={category}>{category}</p>
+        <p className="BookTitle">{title}</p>
+        <p className="BookAuthor">{author}</p>
+        <button type="button" className="Remove" onClick={remove}>Remove</button>
       </div>
       <div className="graphContainer">
         <div className="percentageContainer" />
@@ -20,10 +32,17 @@ function BookItem() {
         <div className="progressContainer" />
         <p>Current Chapter</p>
         <p>Chapter 17</p>
-        <button type="button" className="progressBtn btn">Update Progress</button>
+        <button type="button" className="ProgressBtn btn">Update Progress</button>
       </div>
     </div>
   );
 }
+
+BookItem.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+};
 
 export default BookItem;
--- src/components/BookPage.js
+++ src/components/BookPage.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import BookItem from './BookItem';
 import AddForm from './Form';
+import { useSelector } from 'react-redux';
 
 function BookPage() {
+  const myBooks = useSelector((store) => store.booksReducer);
   return (
     <div>
-      <BookItem />
+      {
+        myBooks.map(
+          (book) => (<BookItem
+            key={book.id}
+            id={book.id}
+            title={book.title}
+            author={book.author}
+            category={book.category}
+          />),
+        )
+      }
       <AddForm />
     </div>
   );
